f46a90bc0d2a723e382eaff729eb6e355097246a
Lisa Mizuno <lisa.mizuno@me.com>
2021-05-28 23:14:26 +0900

    Initial Commit

---
+++ MemoPad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemoPad
+//
+//  Created by Lisa Mizuno on 2021/05/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
